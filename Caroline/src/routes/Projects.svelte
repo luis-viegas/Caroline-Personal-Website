d8482088781c6c96294ac02362f6d6e84df4f1c2
luis-viegas <luiscarlosviegas@icloud.com>
2023-03-05 17:46:02 +0000

    Update Projects.svelte

--- Caroline/src/routes/Projects.svelte
+++ Caroline/src/routes/Projects.svelte
@@ -2,7 +2,6 @@
 	import Project from './ProjectCard.svelte';
 
 	export let posts;
-	console.log(posts);
 
 	function limit_description_name_length(name: string) {
 		if (name.length > 250) {
@@ -16,7 +15,7 @@
 <div class="px-14 text-left mt-28">
 	<h1 class="text-5xl font-semibold md:text-7xl" id="projects">Projects</h1>
 	<hr class="´border-solid border-y-1 border-black mt-1" />
-	<div class="mt-14 md:mt-0 pt-14 grid md:rid-cols-2 grid-cols-1 gap-8">
+	<div class="mt-14 md:mt-0 pt-14 grid md:grid-cols-2 grid-cols-1 gap-8">
 		{#each posts as post, index}
 			<Project
 				title={post.properties.Title.title[0].plain_text}
